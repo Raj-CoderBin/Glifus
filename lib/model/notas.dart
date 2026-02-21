e39f75d184913b0237ecb556e1bf100159e5d17b
Raj <rubensjesusmed@gmail.com>
2026-02-21 12:55:10 -0300

    create db

---
+++ lib/model/notas.dart
@@ -0,0 +1,25 @@
+class Notas {
+  //Atributos
+  int _id;
+  String _nome;
+  String _assunto;
+  String _data;
+
+  // getters and setters
+
+  int get id => this._id;
+
+  set id(int value) => this._id = value;
+
+  get nome => this._nome;
+
+  set nome(value) => this._nome = value;
+
+  get assunto => this._assunto;
+
+  set assunto(value) => this._assunto = value;
+
+  get data => this._data;
+
+  set data(value) => this._data = value;
+}
