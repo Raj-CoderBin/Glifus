859aa3af57ff763077ba614b296f18dd59b71b32
Raj <rubensjesusmed@gmail.com>
2026-02-21 10:32:46 -0300

    Criando janela para Salvar nota

--- lib/home.dart
+++ lib/home.dart
@@ -8,6 +8,59 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController txtnome = TextEditingController();
+  TextEditingController txtassunto = TextEditingController();
+  TextEditingController txtdata = TextEditingController();
+
+  void exibirteladecadastro() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Cadastrar nova nota'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: txtnome,
+                autofocus: true,
+                decoration: InputDecoration(
+                  labelText: 'Nome',
+                  hintText: 'Digite o nome da nota',
+                ),
+              ),
+              TextField(
+                controller: txtassunto,
+                autofocus: true,
+                decoration: InputDecoration(
+                  labelText: 'Assunto',
+                  hintText: 'Digite o assunto',
+                ),
+              ),
+              TextField(
+                controller: txtdata,
+                autofocus: true,
+                decoration: InputDecoration(
+                  labelText: 'Data',
+                  hintText: 'Digite a data',
+                ),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancelar'),
+            ),
+            TextButton(onPressed: () {}, child: Text('Salvar')),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +70,9 @@ class _HomeState extends State<Home> {
       ),
       body: Container(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          exibirteladecadastro();
+        },
         backgroundColor: Colors.orange,
         foregroundColor: Colors.white,
         child: Icon(Icons.add),
